eed198dc028693733cd5a5dc4b313b619f916b5a
Joon Jang <joon.jang95@gmail.com>
2018-09-05 18:14:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ function deepestChild() {
   while (current) {
     
     if (Array.isArray(current)) {
-      
       for (let i = 0; i < current.length; i++) {
       next.push(current[i])
     }
