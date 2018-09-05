7fe83aa93cbf2ca39229d931bde29a421d329aab
Joon Jang <joon.jang95@gmail.com>
2018-09-05 16:32:14 +0000

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ function nestedTarget() {
   return document.querySelector(`div.target`)
 }
 
-function increaseRankBy() {
-  const 
+function increaseRankBy(n) {
+  const hey = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  
+  for (let i = 0; i < hey.length; i++) {
+    hey[i].innerHTML = parseInt(hey[i].innerHTML) + n
+  }
+  return hey
 }
 
 function deepestChild() {
