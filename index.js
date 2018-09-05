b0d02b7e7f22be7cb8d68feb566fdd511ef5116d
Joon Jang <joon.jang95@gmail.com>
2018-09-05 17:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector(`div.target`)
 }
 
-<<<<<<< HEAD
+
 function increaseRankBy(n) {
   const hey = document.getElementById("app").querySelectorAll("ul.ranked-list li")
   
